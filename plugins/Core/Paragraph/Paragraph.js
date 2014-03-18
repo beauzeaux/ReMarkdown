d827d62fdfd882fee6cc0aab76f853317b32c734
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-18 18:28:35 -0400

    Fixed capitalization error in inclusion list.

--- plugins/Core/Paragraph/Paragraph.js
+++ plugins/Core/Paragraph/Paragraph.js
@@ -1,16 +1,15 @@
-define(['ReMarkdown/Plugins/Core/Paragraph/ParagraphElement'],
+define(['ReMarkdown/plugins/Core/Paragraph/ParagraphElement'],
     function (ParagraphElement) {
-        var manifest = {
+        return manifest = {
             name: "Paragraph",
             grammar: {
                 preludes: [],
                 blocks: [], //already defined in the template file
                 spans: [],
-                grammar: null, //grammar is defined in the template
+                grammar: null //grammar is defined in the template
             },
             renderers: {
                 Paragraph: ParagraphElement
             }
         };
-        return manifest;
     });

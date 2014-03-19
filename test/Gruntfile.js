537c37569d4d2b3d5af624e694bb61c54f5e56e0
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-19 14:06:38 -0400

    Refactored lib out of the project and added bower files
    Fixed the use of 'ReMarkdown' path in files

---
+++ test/Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-qunit-junit');
+    grunt.loadNpmTasks('grunt-plato');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        qunit: {
+            all: {
+                options: {
+                    urls: [
+                        'http://localhost:8002/test/all.html'
+                    ],
+                    timeout: 10000
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8002,
+                    base: '.',
+                    hostname: '*'
+                }
+            }
+        },
+        plato: {
+            ReMarkdown: {
+                options: {
+                    exclude: /^test\/lib\//
+                },
+                files: {
+                    '_build/plato': ['test/**/*.js', 'PluginLoader/**/*.js', 'ElementFactory/**/*.js', 'ReMarkdown.js',
+                        'Utils.js']
+                }
+            }
+        }
+    });
+    grunt.registerTask('test', ['connect', 'qunit_junit', 'qunit']);
+    grunt.registerTask('jenkins', ['test', 'plato']);
+};

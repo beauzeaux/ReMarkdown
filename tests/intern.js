a4c009ea55bb8744e01460ec7d229d1cf2e1fc21
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-22 22:29:41 -0400

    Changed some settings for Saucelabs

--- tests/intern.js
+++ tests/intern.js
@@ -24,7 +24,10 @@ define(
             // Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
             // automatically
             capabilities: {
-                'selenium-version': '2.37.0'
+                'selenium-version': '2.37.0',
+                'idle-timeout': 60,
+                'max-duration': 90,
+                'video-upload-on-pass': false
             },
 
             // Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce

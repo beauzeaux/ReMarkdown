071175175ad9fe0a1acce4af27cf3c75c9d0e5b8
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-24 00:35:47 -0400

    Fixed when usage in DocumentElement.js (Core)

--- src/plugins/Core/Document/DocumentElement.js
+++ src/plugins/Core/Document/DocumentElement.js
@@ -3,7 +3,7 @@ define(
     function (dojoString, template, when) {
         return function (promise) {
             //wait until every one of the inlines is done rendering
-            var ret = when(promise, function (result) {
+            var ret = when(promise).then(function (result) {
                 var ret = dojoString.substitute(template, {content: result});
                 return ret;
             });

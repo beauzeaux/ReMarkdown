73606dceb8b63e03c1ecc867f28c30c1f415dbf6
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-29 15:15:49 -0400

    Updated Core to include subtests for Document, Paragraph, and Blocks

--- tests/plugins/Core/Core.js
+++ tests/plugins/Core/Core.js
@@ -10,7 +10,8 @@ define([
     "ReMarkdown/pegjs",
     //These are just to load the individual tests!
     "./Paragraph",
-    "./Document"
+    "./Document",
+    "./Blocks"
 ], function (registerSuite, assert, PluginLoader, ElementFactory, Deferred, all, dojoXML, xmlCmp, pegjs) {
     registerSuite({
         name: 'Core Plugins',
@@ -54,31 +55,6 @@ define([
                 assert('Paragraph' in renderers, "Paragraph Renderer Registered");
                 assert('Document' in renderers, "Document Renderer Registered");
                 assert('Blocks' in renderers, "Blocks Renderer Registered");
-            });
-        },
-        "Sync Rendering": function () {
-            var dfd = this.async(10000, 2);
-            efp.then(function (ef) {
-                var sync = ef.element('Sync', 'Test');
-                var pRes = ef.element('Paragraph', [sync]);
-                pRes.then(dfd.callback(function (result) {
-                    var expected = "<p>Sync:Test</p>";
-                    var a = dojoXML.parse(expected);
-                    var b = dojoXML.parse(result);
-                    var cmp = new xmlCmp();
-                    assert.ok(cmp.areEqual(a, b), "Paragraph renders correctly");
-                }));
-
-                var dRes = ef.element('Document', sync);
-                dRes.then(dfd.callback(function (result) {
-                    var expected = "<!DOCTYPE html><html><head><title></title></head><body>Sync:Test</body></html>";
-                    var a = dojoXML.parse(expected);
-                    var b = dojoXML.parse(result);
-                    var cmp = new xmlCmp();
-                    var ret = cmp.areEqual(a, b);
-                    assert(ret, "Document renders correctly");
-                }));
-
             });
         }
     });

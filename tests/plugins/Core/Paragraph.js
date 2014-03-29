86695ea700b52a90cafdb452c97a8d845e57fd65
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-29 15:16:09 -0400

    Added tests for paragraph plugin

--- tests/plugins/Core/Paragraph.js
+++ tests/plugins/Core/Paragraph.js
@@ -1,82 +1,15 @@
 define([
     "intern!object",
     "intern/chai!assert",
-    "ReMarkdown/PluginLoader",
-    "ReMarkdown/ElementFactory",
-    "dojo/Deferred",
-    "dojo/promise/all",
-    "xml/parser",
-    "xml/equal",
-    "ReMarkdown/pegjs"
-], function (registerSuite, assert, PluginLoader, ElementFactory, Deferred, all, dojoXML, xmlCmp, pegjs) {
-    registerSuite({
-        name: 'Paragraph',
-        setup: function () {
-            pl = new PluginLoader(["TestPlugins/TestPlugins", "Core/Paragraph/Paragraph"]);
-            efp = pl.renderers().then(function (renderers) {
-                return new ElementFactory(renderers);
-            });
-        },
-        "Creation": function () {
-            assert(pl != null, "New Creates a non-null plugin loader");
-        },
-        "Valid Grammar": function () {
-            var dfd = this.async(100000);
-            parser = pl.grammar();
-            parser.then(function (grammar) {
-                var ret;
-                //Don't crash the testing suite with thrown errors
-                try {
-                    ret = pegjs.buildParser(grammar);
-                    return ret;
-                } catch (ex) {
-                    ret = null;
-                    return ret;
-                }
-            });
-            parser.then(dfd.callback(function (parser) {
-                    assert(parser != null, "Grammar is valid");
-                }
-            ));
-        },
-        "Plugin Manifest": function () {
-            return pl.manifest().then(function (manifest) {
-                assert(manifest.grammar.blocks.indexOf("Paragraph") == -1,
-                    "Sets Paragraph Rule" + "\n" + manifest.grammar.blocks);
-            });
-        },
-        "Element Renderers": function () {
-            return pl.renderers().then(function (renderers) {
-                assert('Paragraph' in renderers, "Paragraph Renderer Registered");
-            });
-        },
-        "Sync Rendering": function () {
-            var dfd = this.async(10000, 1);
-            efp.then(function (ef) {
-                var sync = ef.element('Sync', 'Test');
-                var pRes = ef.element('Paragraph', [sync]);
-                pRes.then(dfd.callback(function (result) {
-                    var expected = "<p>Sync:Test</p>";
-                    var a = dojoXML.parse(expected);
-                    var b = dojoXML.parse(result);
-                    var cmp = new xmlCmp();
-                    assert.ok(cmp.areEqual(a, b), "Paragraph renders correctly");
-                }));
-            });
-        },
-        "Async Rendering": function () {
-            var dfd = this.async(10000, 1);
-            efp.then(function (ef) {
-                var async = ef.element('Async', 'Test');
-                var pRes = ef.element('Paragraph', [async]);
-                pRes.then(dfd.callback(function (result) {
-                    var expected = "<p>Async:Test</p>";
-                    var a = dojoXML.parse(expected);
-                    var b = dojoXML.parse(result);
-                    var cmp = new xmlCmp();
-                    assert.ok(cmp.areEqual(a, b), "Paragraph renders correctly");
-                }));
-            });
+    "../BasePluginTester"
+], function (registerSuite, assert, PluginTester) {
+    PluginTester.TestPlugin({
+        name: "Paragraph",
+        paths: ["Core/Paragraph/Paragraph"],
+        blocks: [],
+        spans: [],
+        renderers: {
+            "Paragraph": "<p>${content}</p>"
         }
     });
 });

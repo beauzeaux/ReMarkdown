5444e276f560e17c74fecc589154f76cbf99ffe5
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-29 19:15:30 -0400

    Added nested quote tests

--- tests/plugins/Base/quote/main.js
+++ tests/plugins/Base/quote/main.js
@@ -4,7 +4,11 @@ define([
     "../../BasePluginTester",
     "dojo/text!./1_in.rm",
     "dojo/text!./1_out.html",
-], function (registerSuite, assert, PluginTester, test1in, test1out) {;
+    "dojo/text!./2_in.rm",
+    "dojo/text!./2_out.html"
+], function (registerSuite, assert, PluginTester,
+             test1in, test1out, test2in, test2out
+    ){
     PluginTester.TestPlugin({
         name: "Quote",
         paths: ["Base/quote/quote"],
@@ -18,7 +22,8 @@ define([
         name: "Quote",
         paths: ["Base/quote/quote"],
         tests:[
-            [test1in, test1out]
+            [test1in, test1out],
+            [test2in, test2out]
         ]
     })
 });

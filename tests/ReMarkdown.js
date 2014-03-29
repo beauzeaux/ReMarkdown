5f9b7497e9fb773a15e77e691ed2e7f62955f472
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-29 15:12:00 -0400

    Added Core tests for ReMarkdown parser

--- tests/ReMarkdown.js
+++ tests/ReMarkdown.js
@@ -1,13 +1,31 @@
 define([
     "intern!object",
     "intern/chai!assert",
-    "ReMarkdown"
-], function (registerSuite, assert, ReMarkdown) {
-    var parser = new ReMarkdown();
+    "ReMarkdown",
+    "xml/parser",
+    "xml/equal"
+], function (registerSuite, assert, ReMarkdown, dojoXML, xmlCmp) {
     registerSuite({
-        name: 'ReMarkdown Sanity Checks',
-        create: function () {
+        name: 'ReMarkdown Parser',
+        setup: function () {
+            parser = ReMarkdown({pluginList: ["Core/Core"]});
+        },
+        "Creation": function () {
             assert.notEqual(parser, null, "Constructor returns a valid object");
+        },
+        "Core Parsing": function () {
+            var dfd = this.async(10000);
+            var test = "Hello World\n";
+            var expected = "<!DOCTYPE html><html><head>" +
+                "<title></title></head><body><p>Hello World</p></body></html>"
+            var promise = parser.parse(test);
+            promise.then(dfd.callback(function (result) {
+                var a = dojoXML.parse(expected);
+                var b = dojoXML.parse(result);
+                var cmp = new xmlCmp();
+                var ret = cmp.areEqual(a, b);
+                assert(ret, "Paragraph parses correctly");
+            }));
         }
     });
 });

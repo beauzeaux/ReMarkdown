636991ed8c63fe6aa79c7835c903317366c0a11e
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-29 16:23:22 -0400

    Added Base plugin meta-plugin

---
+++ src/plugins/Base/main.js
@@ -0,0 +1,17 @@
+/**
+ * Meta-Manifest file for all the core Elements for the
+ * ReMarkdown Syntax. Included in the package are
+ *
+ * - Blocks
+ * - Documents
+ * - Paragraphs
+ */
+define(["ReMarkdown/PluginLoader", "dojo/when"],
+    function (PluginLoader, when) {
+        var plugins = [
+            "Base/quote/quote",
+        ];
+        var pLoader = new PluginLoader(plugins);
+        var promise = pLoader.manifest();
+        return promise;
+    });

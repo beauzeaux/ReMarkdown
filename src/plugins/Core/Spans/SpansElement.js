99d81c0e5de4bc4e81d7f53ce0e304192de780a4
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-30 13:45:41 -0400

    Fixed inane debug message from span elements

--- src/plugins/Core/Spans/SpansElement.js
+++ src/plugins/Core/Spans/SpansElement.js
@@ -1,7 +1,6 @@
 define(['dojo/string', 'dojo/promise/all'],
     function (dojoString, all) {
         return function (promises) {
-            console.log("SPANSSS");
             //flatten the promises since it is an array of arrays
             var merged = [].concat.apply([], promises);
             var ret =  all(merged).then(function (strs) {

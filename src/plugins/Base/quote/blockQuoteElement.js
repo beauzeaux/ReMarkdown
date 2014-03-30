41fd4bfa46e313b2258fcf4c78a08017d06d8cdd
Nick Beaulieu <nsbeauli@princeton.edu>
2014-03-30 14:26:13 -0400

    Fixed typo

--- src/plugins/Base/quote/blockQuoteElement.js
+++ src/plugins/Base/quote/blockQuoteElement.js
@@ -1,7 +1,7 @@
 define(['dojo/string', 'dojo/text!./blockQuote.html', 'dojo/promise/all'],
     function (string, template, all) {
         return function (pText) {
-            var ret = ZpText.then(function (text) {
+            var ret = pText.then(function (text) {
                 return string.substitute(template, {content: text});
             });
             return ret;

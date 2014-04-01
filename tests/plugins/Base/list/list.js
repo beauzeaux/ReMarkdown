3d92f957d24fc44d37b99bf7c42627d38cbd57f2
Nick Beaulieu <nsbeauli@princeton.edu>
2014-04-01 16:09:11 -0400

    Updated list tests

--- tests/plugins/Base/list/list.js
+++ tests/plugins/Base/list/list.js
@@ -2,7 +2,29 @@ define([
     "intern!object",
     "intern/chai!assert",
     "../../BasePluginTester",
-], function (registerSuite, assert, PluginTester){
+    "dojo/text!./1_in.rm",
+    "dojo/text!./1_out.html",
+    "dojo/text!./2_in.rm",
+    "dojo/text!./2_out.html",
+    "dojo/text!./3_in.rm",
+    "dojo/text!./3_out.html",
+    "dojo/text!./4_in.rm",
+    "dojo/text!./4_out.html",
+    "dojo/text!./5_in.rm",
+    "dojo/text!./5_out.html",
+    "dojo/text!./6_in.rm",
+    "dojo/text!./6_out.html"
+
+
+], function (registerSuite, assert, PluginTester,
+             test1in, test1out,
+             test2in, test2out,
+             test3in, test3out,
+             test4in, test4out,
+             test5in, test5out,
+             test6in, test6out
+
+    ){
     PluginTester.TestPlugin({
         name: "List",
         paths: ["Base/list/list"],
@@ -14,14 +36,18 @@ define([
             "OrderedList" : '<ol>${content}</ol>'
         }
     });
-    /*
+
     PluginTester.TestParse({
-        name: "Quote",
-        paths: ["Base/list/list"],
+        name: "List",
+        paths: ["Base/list/list", "Base/quote/quote"],
         tests:[
             [test1in, test1out],
-            [test2in, test2out]
+            [test2in, test2out],
+            [test3in, test3out],
+            [test4in, test4out],
+            [test5in, test5out],
+            [test6in, test6out]
         ]
     });
-    */
+
 });
